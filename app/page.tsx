de38e4ca6bca24584aa13577c065ed4a710a117f
patey <patey@sio.ndlp>
2025-11-28 12:01:23 +0100

    montrer question sur site

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,42 @@
 "use client"
 
-import { useEffect } from 'react';
+
+
+import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
 
-export default function Home() {
+
+
+export default function Home(){
+  const [question, setQuestion] = useState<any>(null);
   useEffect(() => {
     async function fetchQuestion() {
       const { data, error } = await supabase
         .from('question')
-        .select('*')
-        .limit(1);
+        .select('*');
 
       if (error) console.error(error);
-      else console.log(data);
+      else setQuestion(data[0]); // On stocke la première question dans l’état
     }
+  
 
     fetchQuestion();
   }, []);
+ 
 
-  return <h1>Bienvenue sur CyberQuiz</h1>;
+ return (
+  <div>
+    <h1>Bienvenue sur CyberQuiz</h1>
+
+    {question ? (
+      <div>
+        <h2>{question.texte}</h2> 
+      </div>
+    ) : (
+      <p>Chargement de la question...</p>
+    )}
+
+  </div>
+)
+
 }
